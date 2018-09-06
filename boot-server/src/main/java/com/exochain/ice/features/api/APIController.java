4f03426bf3bca43136a57f9eedbc10aa2011d153
Luna <lucidlyluna@gmail.com>
2018-09-06 13:17:32 -0400

    Add testing REST API Controller

---
+++ boot-server/src/main/java/com/exochain/ice/features/api/APIController.java
@@ -0,0 +1,48 @@
+package com.exochain.ice.features.homepage;
+
+import com.fasterxml.uuid.Generators;
+import com.fasterxml.uuid.impl.TimeBasedGenerator;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import javax.servlet.http.HttpSession;
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.util.Locale;
+
+class TestObject {
+    public String hello;
+    public String there;
+    public String friend;
+}
+
+@RestController
+@SessionAttributes(names = {"visitor"})
+@RequestMapping("/api/v1")
+public class APIController {
+    // Just for demonstration purposes of adding a UUID to Session and Tracking
+    private static final TimeBasedGenerator UUID_GENERATOR = Generators.timeBasedGenerator();
+
+    @ModelAttribute("visitor")
+    public TestVisitor createTestVisitorObject(HttpSession httpSession) {
+        Instant i = Instant.ofEpochMilli(httpSession.getCreationTime());
+        ZonedDateTime sessionStart = ZonedDateTime.ofInstant(i, ZoneOffset.UTC);
+
+        return new TestVisitor(UUID_GENERATOR.generate().toString(), sessionStart);
+    }
+
+    @RequestMapping(value = "/visitor", method = RequestMethod.GET)
+    public TestObject homePage(Locale locale, Model model) {
+        TestObject test = new TestObject();
+        test.hello = "Hello ";
+        test.there = "there, ";
+        test.friend = "friend.";
+        return test;
+    }
+}
